1e194032756987b38407fc42a462da4ebbf75680
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-09-09 00:47:29 +0600

    lets vocabulary title and button added

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,1 +1,25 @@
-const loadLessons
+const loadLessons = () => {
+    fetch('https://openapi.programming-hero.com/api/levels/all') // promise for response
+        .then(res => res.json()) // promise of json data
+        .then(json => displayLessons(json.data))
+}
+
+const displayLessons = (lessons) => {
+    // 1. get the container & empty
+    const levelContainer = document.getElementById('level-container')
+    levelContainer.innerHTML = ''
+
+    // 2. get into every lessons
+    lessons.forEach(lesson => {
+
+        // 3. create elements
+        const btnDiv = document.createElement('div')
+        btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+                            </button>`
+
+        // 4. append into container
+        levelContainer.appendChild(btnDiv)
+    })
+}
+
+loadLessons()
