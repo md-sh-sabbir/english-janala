4541cf6f3ffbda697822b35e416fa92e969fee30
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-09-11 00:09:48 +0600

    dynamic word meaning data added and implimented with empty label data

--- assests/js/index.js
+++ assests/js/index.js
@@ -13,12 +13,33 @@ const loadLevelWord = (id) => {
 
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container')
-    // wordContainer.innerHTML = ''
+    wordContainer.innerHTML = ''
+
+    if(words.length == 0){
+        wordContainer.innerHTML = `
+             <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-[Hind_Siliguri]">
+                    <img src="assests/images/alert-error.png" alt="" class="mx-auto">
+                    <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                    <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+            </div>
+        `
+        return
+    }
 
     words.forEach(word => {
-        // console.log(word)
+        console.log(word)
         const card = document.createElement('div')
-        card.innerHTML = `<p> Cat </p>`
+        card.innerHTML = `<div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+                <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+                <p class="font-semibold">Meaning / Pronounciation</p>
+                <div class="font-[Hind_Siliguri]">
+                    <h2 class="text-2xl font-medium text-gray-600">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</h2>
+                </div>
+                <div class="flex justify-between items-center">
+                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>`
 
         wordContainer.appendChild(card)
     })
