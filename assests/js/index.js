3ef656fa1c7158257f7833a078b248a2008e2e58
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-09-11 02:12:27 +0600

    modal functionality added throught api

--- assests/js/index.js
+++ assests/js/index.js
@@ -25,6 +25,38 @@ const loadLevelWord = (id) => {
     })
 }
 
+const loadWordDetail = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`
+    const res = await fetch(url)
+    const details = await res.json()
+    displayWordDetails(details.data)
+}
+
+const displayWordDetails = (word) => {
+    console.log(word)
+    const detailsBox = document.getElementById('details-container')
+    detailsBox.innerHTML = `
+                            <div class="">
+                                <h2 class="text-2xl font-bold">${word.word} (<i class="fa-solid fa-microphone-lines"></i>  :${word.pronunciation})</h2>
+                            </div>
+                            <div class="">
+                                <h2 class="font-bold">Meaning</h2>
+                                <p>${word.meaning}</p>
+                            </div>
+                            <div class="">
+                                <h2 class="font-bold">Example</h2>
+                                <p>${word.sentence}</p>
+                            </div>
+                            <div class="">
+                                <h2 class="font-bold">Synonym</h2>
+                                <span class="btn">Syn1</span>
+                                <span class="btn">Syn2</span>
+                                <span class="btn">Syn3</span>
+                            </div>
+                        `
+    document.getElementById('word_modal').showModal()
+}
+
 const displayLevelWord = (words) => {
     const wordContainer = document.getElementById('word-container')
     wordContainer.innerHTML = ''
@@ -50,7 +82,7 @@ const displayLevelWord = (words) => {
                     <h2 class="text-2xl font-medium text-gray-600">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</h2>
                 </div>
                 <div class="flex justify-between items-center">
-                    <button onclick="my_modal_5.showModal()" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
             </div>`
