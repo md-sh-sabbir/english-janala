7c1376c9a71d1c8c569d265b87e4cbc16bc9ced1
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-09-10 17:47:32 +0600

    words card template added

--- assests/js/index.js
+++ assests/js/index.js
@@ -4,6 +4,26 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById('word-container')
+    // wordContainer.innerHTML = ''
+
+    words.forEach(word => {
+        // console.log(word)
+        const card = document.createElement('div')
+        card.innerHTML = `<p> Cat </p>`
+
+        wordContainer.appendChild(card)
+    })
+}
+
 const displayLessons = (lessons) => {
     // 1. get the container & empty
     const levelContainer = document.getElementById('level-container')
@@ -14,7 +34,8 @@ const displayLessons = (lessons) => {
 
         // 3. create elements
         const btnDiv = document.createElement('div')
-        btnDiv.innerHTML = `<button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+        btnDiv.innerHTML = `<button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+                            <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
                             </button>`
 
         // 4. append into container
