26bc36246936120394b64f456ab40fde98c09b69
md-sh-sabbir <sabbirhossain9870@gmail.com>
2025-09-11 21:16:36 +0600

    pronouncition feature added through voloume button on vocabulary card

--- assests/js/index.js
+++ assests/js/index.js
@@ -3,6 +3,12 @@ const createElements = (arr) => {
     return htmlElements.join(' ')
 }
 
+function pronounceWord(word){
+    const utterance = new SpeechSynthesisUtterance(word)
+    utterance.lang = "en-EN" // English
+    window.speechSynthesis.speak(utterance)
+}
+
 const manageSpinner = (status) => {
     if(status == true){
         document.getElementById('spinner').classList.remove('hidden')
@@ -98,7 +104,7 @@ const displayLevelWord = (words) => {
                 </div>
                 <div class="flex justify-between items-center">
                     <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-circle-info"></i></button>
-                    <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-volume-high"></i></button>
+                    <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] text-[#374957]"><i class="fa-solid fa-volume-high"></i></button>
                 </div>
             </div>`
 
@@ -131,7 +137,7 @@ loadLessons()
 
 document.getElementById('btn-search').addEventListener('click', () => {
     removeActive()
-    
+
     const input = document.getElementById('input-search')
     const searchValue = input.value.trim().toLowerCase()
     console.log(searchValue)
